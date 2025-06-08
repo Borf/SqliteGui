b5321d3a7adb499caf24827b0afea627c0d32334
Borf <borfje@gmail.com>
2025-06-08 02:46:08 +0200

    First version

---
+++ SqliteGui/Database.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SqliteGui;
+
+public class Database : IDisposable
+{
+    private string fileName;
+    private SqliteConnection connection;
+
+    public List<string> Tables { get; set; } = new();
+
+    public Database(string fileName)
+    {
+        this.fileName = fileName;
+        connection = new SqliteConnection($"Data Source='{fileName}'");
+        connection.Open();
+        RefreshTables();
+
+        if(!Tables.Contains("_gui_tablename"))
+        {
+            using var command = connection.CreateCommand();
+            command.CommandText = @"CREATE TABLE _gui_tablename (TableName	TEXT NOT NULL,	NameColumn	TEXT NOT NULL,	PRIMARY KEY(TableName))";
+            command.ExecuteNonQuery();
+        }
+
+    }
+
+
+    public void RefreshTables()
+    {
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT name FROM sqlite_master WHERE type='table'";
+        using var reader = command.ExecuteReader();
+        Tables = new();
+        while (reader.Read())
+        {
+            Tables.Add(reader.GetString(0));
+        }
+    }
+
+
+    public void Dispose()
+    {
+        connection.Dispose();
+    }
+
+    public List<List<object?>> RefreshTableData(string tableName, int offset)
+    {
+        List<List<object?>> data = new();
+        using var command = connection.CreateCommand();
+        command.CommandText = $"SELECT * FROM {tableName} LIMIT {offset*20},20";
+        using var reader = command.ExecuteReader();
+        //var columnScheme = reader.GetColumnSchema();
+        //foreach (var column in columnScheme)
+        //{
+        //    browseTable.Columns.Add(column.ColumnName);
+        //}
+        while(reader.Read())
+        {
+            List<object?> row = new();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                row.Add(reader.GetValue(i));
+            }
+            data.Add(row);
+        }
+        return data;
+    }
+
+    public string GetCreateTable(string selectedTable)
+    {
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT sql FROM sqlite_master WHERE type='table' and name='"+selectedTable+"'";
+        using var reader = command.ExecuteReader();
+        if (reader.Read())
+            return reader.GetString(0);
+        return "";
+    }
+
+    public List<TableRelation> GetTableRelations(string tableName)
+    {
+        var relations = new List<TableRelation>();
+        string createTableSql = GetCreateTable(tableName);
+
+        if (!string.IsNullOrEmpty(createTableSql))
+        {
+            var foreignKeyMatches = Regex.Matches(createTableSql, @"FOREIGN KEY\s*\(([^)]+)\)\s*REFERENCES\s*([^\(]+)\s*\(([^)]+)\)(.*)", RegexOptions.IgnoreCase);
+            foreach (Match match in foreignKeyMatches)
+            {
+                if (match.Groups.Count > 4)
+                {
+                    var relation = new TableRelation
+                    {
+                        Field = match.Groups[1].Value.Trim(),
+                        ReferencedTable = match.Groups[2].Value.Trim(),
+                        ReferencedField = match.Groups[3].Value.Trim(),
+                        CascadingEffect = match.Groups[4].Value.Trim()
+                    };
+                    relations.Add(relation);
+                }
+            }
+        }
+
+        return relations;
+    }
+
+    public List<Column> GetTableStructure(string tableName)
+    {
+        if (tableName == "")
+            return new();
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM " + tableName + " LIMIT 1";
+        using var reader = command.ExecuteReader();
+        return reader.GetColumnSchema().Select(c => new Column
+        {
+            ColumnName = c.ColumnName,
+            DataType = c.DataTypeName,
+            AllowDBNull = c.AllowDBNull,
+            IsKey = c.IsKey ?? false,
+            IsAutoIncrement = c.IsAutoIncrement ?? false,
+            IsReadOnly = c.IsReadOnly ?? false,
+            IsUnique = c.IsUnique ?? false,
+            Size = c.ColumnSize ?? 0,
+        }).ToList();
+    }
+
+    public List<DbIndex> GetIndices(string tableName)
+    {
+        var indices = new List<DbIndex>();
+        if (tableName == "")
+            return indices;
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM sqlite_master WHERE type='index' AND tbl_name = '" + tableName + "' LIMIT 1";
+        using var reader = command.ExecuteReader();
+        while(reader.Read())
+        {
+            indices.Add(new DbIndex(reader.IsDBNull(4) ? string.Empty : reader.GetString(4))
+            {
+                Name = reader.GetString(1),
+                Table = reader.GetString(2),
+            });
+        }
+        return indices;
+    }
+
+    public int PageCount(string table)
+    {
+        using var command = connection.CreateCommand();
+        command.CommandText = $"SELECT count(*) FROM {table}";
+        using var reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            return reader.GetInt32(0)/20;
+        }
+        return 0;
+    }
+}
+
+public class DbIndex
+{
+    public required string Name { get; set; }
+    public required string Table { get; set; }
+    public string Sql { get; set; }
+
+    public string Field { get; private set; } = string.Empty;
+    public DbIndex(string Sql)
+    {
+        this.Sql = Sql;
+        var match = Regex.Match(Sql.Replace("\r", "").Replace("\n", ""), @"CREATE INDEX ""(.*)"" ON ""(.*)"" \((.*)\)");
+        if (match.Success)
+        {
+            var fields = match.Groups[3].Value;
+            fields = fields[1..(fields.Length - 1)];
+            Field = string.Join(", ", fields.Split("\", \""));
+        }
+        else
+            Field = "ERROR";
+    }
+}
+
+
+public class TableRelation
+{
+    public string Field { get; set; }
+    public string ReferencedTable { get; set; }
+    public string ReferencedField { get; set; }
+    public string CascadingEffect { get; set; }
+}
+
+
+public class Column
+{
+    public string ColumnName = string.Empty;
+    public string DataType = string.Empty;
+    public bool? AllowDBNull;
+    public bool IsKey;
+    public bool IsAutoIncrement;
+    public bool IsReadOnly;
+    public bool IsUnique;
+    public int Size;
+}
--- SqliteGui/Program.cs
+++ SqliteGui/Program.cs
@@ -1,2 +1,200 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConGui;
+using ConGui.Util;
+using SqliteGui;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Diagnostics;
+
+Gui.CreateContext();
+
+
+Database database = new Database("database.db");
+string SelectedTable = string.Empty;
+List<Column> SelectedTableStructure = new();
+List<List<object?>> SelectedTableData = new();
+int SelectedTableBrowsePage = 0;
+int SelectedTablePageCount = 0;
+
+string text = "Hello";
+
+while (true)
+{
+    Gui.BeginFrame();
+
+    Gui.Begin("#main", WindowFlags.TopWindow | WindowFlags.HasMenu);
+    {
+        Gui.BeginMenuBar();
+        if (Gui.BeginMenu("File"))
+        {
+            if (Gui.MenuItem("Open"))
+                Debug.WriteLine("Open clicked");
+            if (Gui.MenuItem("Save"))
+                Debug.WriteLine("Save clicked");
+            if (Gui.MenuItem("Quit"))
+                Environment.Exit(0);
+            Gui.EndMenu();
+        }
+
+        if (Gui.BeginMenu("View"))
+        {
+            Gui.EndMenu();
+        }
+
+        if (Gui.BeginMenu("Options"))
+        {
+            Gui.EndMenu();
+        }
+        Gui.EndMenuBar();
+
+
+        Gui.Split("Split", true, 30);
+        {
+            Gui.BeginList("Tables");
+            {
+                foreach (var table in database.Tables)
+                {
+                    if (Gui.ListEntry(table, ListChangedEvent.OnSelect))
+                    {
+                        SetTable(table);
+                    }
+                }
+            }
+            Gui.EndList();
+        }
+        Gui.NextSplit();
+        {
+            if (string.IsNullOrEmpty(SelectedTable))
+            {
+                Gui.SetNextTextColor(Style.DangerText);
+                Gui.Text("No table selected");
+            }
+            else
+            {
+                Gui.BeginTabPanel("Tabs");
+
+                if (Gui.BeginTab("Structure"))
+                {
+                    Gui.Text("Name: ");
+                    int column = 0;
+                    foreach (Column field in SelectedTableStructure) 
+                    {
+                        Gui.PushId((column++) + "");
+                        Gui.SetNextWidth(2);
+                        Gui.Text(column + "");
+
+                        Gui.SameLine();
+                        Gui.SetNextWidth(15);
+                        Gui.InputText("#name", false, ref field.ColumnName);
+
+                        Gui.SameLine();
+                        Gui.SetNextWidth(10);
+                        Gui.InputText("#type", false, ref field.DataType);
+
+                        Gui.PopId();
+                    }
+
+                    if(Gui.Button("Add column", true))
+                    {
+                        SelectedTableStructure.Add(new Column { ColumnName = "", DataType = "TEXT" });
+                    }
+
+                    Gui.EndTab();
+                }
+                if (Gui.BeginTab("Browse"))
+                {
+                    int colWidth = 20;
+                    Gui.PushId("Header");
+                    foreach (Column field in SelectedTableStructure)
+                    {
+                        if(field != SelectedTableStructure.First())
+                            Gui.SameLine();
+                        Gui.SetNextWidth(colWidth);
+                        Gui.Text(field.ColumnName);
+                    }
+                    Gui.PopId();
+
+                    int rowId = 0;
+                    foreach(var row in SelectedTableData)
+                    {
+                        Gui.PushId("row" + rowId++);
+                        int col = 0;
+                        for (int i = 0; i < row.Count; i++)
+                        {
+                            object? cell = row[i];
+                            Gui.SetNextWidth(colWidth);
+                            if (i > 0)
+                                Gui.SameLine();
+                            if (cell is string)
+                            {
+                                string value = (string)cell;
+                                if (Gui.InputText("#" + col++, false, ref value))
+                                    cell = (object?)value;
+                            }
+                            else if (cell is int || cell is long)
+                            {
+                                Gui.Text(((long)cell) + "");
+                            }
+                            else
+                            {
+                                Gui.Text(cell.GetType().ToString());
+                            }
+                        }
+
+                        Gui.PopId();
+                    }
+                    Gui.Text("");
+
+                    if(SelectedTablePageCount > 0)
+                    {
+                        if(Gui.Button("Prev", true))
+                        {
+                            SetPage(SelectedTableBrowsePage - 1);
+                        }
+                        Gui.SameLine();
+                        Gui.Text($"Page {SelectedTableBrowsePage+1} of {SelectedTablePageCount+1}");
+                        Gui.SameLine();
+                        if(Gui.Button("Next", true))
+                        {
+                            SetPage(SelectedTableBrowsePage + 1);
+                        }
+                    }
+
+                    Gui.EndTab();
+                }
+                if (Gui.BeginTab("Search"))
+                {
+
+                    Gui.EndTab();
+                }
+
+
+
+                Gui.EndTabPanel();
+            }
+        }
+        Gui.EndSplit();
+    }
+    Gui.End();
+
+
+    Gui.Render();
+
+    await Task.Delay(1);
+}
+
+void SetTable(string table)
+{
+    SelectedTable = table;
+    SelectedTableStructure = database.GetTableStructure(SelectedTable);
+    SelectedTableBrowsePage = 0;
+    SelectedTableData = database.RefreshTableData(table, 0);
+    SelectedTablePageCount = database.PageCount(table);
+}
+
+void SetPage(int newPage)
+{
+    SelectedTableBrowsePage = Math.Clamp(newPage, 0, SelectedTablePageCount);
+    SelectedTableData = database.RefreshTableData(SelectedTable, SelectedTableBrowsePage);
+
+}
